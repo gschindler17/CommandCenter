18b8cdf5e16166a8be87d6ced9ff8ad488a5a274
Garrett Schindler <garrettschindler17@gmail.com>
2022-04-30 20:52:48 -0500

    Persisting File!!
    - Files can be written to
    - Not read yet

--- CommandCenterGarrettSchindler/src/commandCenterPackage/PersistenceBackend.java
+++ CommandCenterGarrettSchindler/src/commandCenterPackage/PersistenceBackend.java
@@ -1,14 +1,83 @@
 package commandCenterPackage;
 
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class PersistenceBackend {
+	
+	
+	private CircularLinkedList<String> RotatingMessageList;
+	
+	private MinHeap PriorityMinHeap;
+	
+	private Stack<String> BacklogStack;
+	
+	private FileWriter writer;
+	
+	private File outputFile;
+	
+	
+	
+	public PersistenceBackend (CircularLinkedList<String> _circularLinkedList, MinHeap _minHeap, Stack<String> _stack) {
+		RotatingMessageList = _circularLinkedList;
+		PriorityMinHeap = _minHeap;
+		BacklogStack = _stack;
+		
+		
+		System.out.println("IN CONSTRUCTOR");	
+		
+		
+		try {
+			File output = new File("garbageFile.txt");
+			PrintWriter writer = new PrintWriter(new FileWriter("DataStoringFile.txt"));
+
+			System.out.println(RotatingMessageList.toString());
+			
+			writer.write("Overwritten?");
+			writer.append(RotatingMessageList.toString());
+			writer.append("\nTHIS IS CONTENT");
+			writer.append(PriorityMinHeap.toString());
+			writer.append(BacklogStack.toString());
+			writer.flush();
+			writer.close();
+			
+		} catch (IOException _exception) {
+			System.out.println("THERE'S AN IO EXCEPTION IN PERSISTENCE BACKEND");
+		}
+		
+		
+				
+	}
+		
 
 	public void saveData() {
-		// TODO Auto-generated method stub
+		
+		try {
+			PrintWriter writer = new PrintWriter(new FileWriter("DataStoringFile.txt"));
+
+			writer.append(RotatingMessageList.toString() + "\n");
+			writer.append(PriorityMinHeap.toString() + "\n");
+			writer.append(BacklogStack.toString() + "\n");
+			writer.flush();
+			writer.close();
+			
+		} catch (IOException _exception) {
+			System.out.println("THERE'S AN IO EXCEPTION IN PERSISTENCE BACKEND");
+		}
+		
 		
 	}
 
 	public void reloadData() {
 		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	public void loadInData() {
 		
 	}
 
